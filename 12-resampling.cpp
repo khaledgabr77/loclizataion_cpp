4f681775ae2439059094cd69d05f2bd23b2468a7
khaled gabr <khaledgabr77@gmial.com>
2020-05-27 04:03:49 +0200

    add resampling

---
+++ 12-resampling.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+double w[] = { 0.6, 1.2, 2.4, 0.6, 1.2 };//You can also change this to a vector
+
+//TODO: Define a  ComputeProb function and compute the Probabilities
+
+double sum = 0;
+
+void ComputeProb(double w[], int n)
+{
+    for (int i = 0; i < n; i++) {
+        sum = sum + w[i];
+    }
+    for (int j = 0; j < n; j++) {
+        w[j] = w[j] / sum;
+        cout << "P" << j + 1 << "=" << w[j] << endl;
+    }
+}
+
+int main()
+{
+    ComputeProb(w, sizeof(w) / sizeof(w[0]));
+    return 0;
+}
+
